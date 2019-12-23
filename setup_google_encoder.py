6f45986af713f3c015c60024f8fb1622bb7c1cb9
manoranjan11 <39557252+manoranjan11@users.noreply.github.com>
2019-12-23 19:10:24 +0530

    Update setup_google_encoder.py

--- setup_google_encoder.py
+++ setup_google_encoder.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="my_LSTM_code",
     version='0.2',
-    install_requires = ['tensorflow-hub','seaborn','tensorflow>=1.7','h5py','numpy','matplotlib','pandas'],
+    install_requires = ['tensorflow-hub','seaborn','h5py','matplotlib','pandas','pickle'],
     scripts=["predictor_google_encoder.py","preprocess_google_encoder.py"])
