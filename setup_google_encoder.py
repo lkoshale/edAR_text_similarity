c1258371a25ef59afa7f1a90434e8989707bd690
manoranjan11 <39557252+manoranjan11@users.noreply.github.com>
2019-12-23 17:56:40 +0530

    Update setup_google_encoder.py

--- setup_google_encoder.py
+++ setup_google_encoder.py
@@ -10,4 +10,5 @@ from setuptools import setup
 setup(
     name="my_LSTM_code",
     version='0.2',
+    install_requires = ['tensorflow-hub','seaborn','tensorflow>=1.7']
     scripts=["predictor_google_encoder.py","preprocess_google_encoder.py"])
